548f2fd52decebe46f74d4a24d7aa093e3629bc5
andrestume <andres.tume@gmail.com>
2024-12-12 20:58:02 -0500

    Update E-Commerce Databse

--- create_database/dsrp_ecommerce_g13.sql
+++ create_database/dsrp_ecommerce_g13.sql
@@ -1,6 +1,123 @@
 CREATE DATABASE dsrp_ecommerce_g13;
 GO
 
-USE dsrp_prestamos_financieros_6;
+USE dsrp_ecommerce_g13;
 GO
 
+
+--Create CLIENTES
+CREATE TABLE clientes(
+id INT PRIMARY KEY IDENTITY(1,1),
+tipo_documento VARCHAR(50) NOT NULL,
+numero_documento VARCHAR(50) UNIQUE NOT NULL,
+apellido_paterno VARCHAR(100) NOT NULL,
+apellido_materno VARCHAR(100) NOT NULL,
+nombre VARCHAR(100) NOT NULL,
+email VARCHAR(255) UNIQUE NOT NULL,
+direccion VARCHAR(255) NOT NULL,
+ciudad VARCHAR(100) NOT NULL,
+departamento VARCHAR(100) NOT NULL,
+telefono VARCHAR(15) NOT NULL,
+fecha_nacimiento DATE NULL,
+fecha_registro DATETIME DEFAULT GETDATE()
+);
+
+--Create CATEGORIAS
+CREATE TABLE categorias (
+id INT PRIMARY KEY IDENTITY(1,1),
+nombre VARCHAR(255) UNIQUE NOT NULL,
+categoria_padre_id INT NULL,
+CONSTRAINT FK_categoria_padre_id FOREIGN KEY (categoria_padre_id) REFERENCES categorias(id)
+);
+
+--Create PRODUCTOS
+CREATE TABLE productos (
+id INT PRIMARY KEY IDENTITY(1,1),
+nombre VARCHAR(255) UNIQUE NOT NULL,
+descripcion TEXT,
+precio DECIMAL(10, 2) NOT NULL,
+stock INT NOT NULL,
+categoria_id INT NOT NULL,
+CONSTRAINT FK_categoria_id_productos FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+--Create TIENDAS
+CREATE TABLE tiendas (
+id INT PRIMARY KEY IDENTITY(1,1),
+nombre VARCHAR(255) UNIQUE NOT NULL,
+direccion VARCHAR(255) NOT NULL,
+ciudad VARCHAR(100) NOT NULL,
+departamento VARCHAR(100) NOT NULL
+);
+
+--Create PEDIDOS
+CREATE TABLE pedidos (
+id INT PRIMARY KEY IDENTITY(1,1),
+cliente_id INT NOT NULL,
+tienda_id INT NOT NULL,
+fecha_pedido DATETIME DEFAULT GETDATE() NOT NULL,
+total DECIMAL(10, 2) NOT NULL,
+estado VARCHAR(50) NOT NULL,
+CONSTRAINT FK_cliente_id_pedidos FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+CONSTRAINT FK_tienda_id_pedidos FOREIGN KEY (tienda_id) REFERENCES tiendas(id)
+);
+
+--Create DETALLES_PEDIDO
+CREATE TABLE detalles_pedido (
+id INT PRIMARY KEY IDENTITY(1,1),
+pedido_id INT NOT NULL,
+producto_id INT NOT NULL,
+cantidad INT NOT NULL,
+precio DECIMAL(10, 2) NOT NULL,
+CONSTRAINT FK_pedido_id_detalles_pedido FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+CONSTRAINT FK_producto_id_detalles_pedido FOREIGN KEY (producto_id) REFERENCES productos(id)
+);
+
+--Create METODO_PAGO
+CREATE TABLE metodo_pago (
+id INT PRIMARY KEY IDENTITY(1,1),
+nombre VARCHAR(50) NOT NULL
+);
+
+--Create PAGOS
+CREATE TABLE Pagos (
+id INT PRIMARY KEY IDENTITY(1,1),
+pedido_id INT NOT NULL,
+metodo_pago_id INT NOT NULL,
+importe DECIMAL(10, 2) NOT NULL,
+estado VARCHAR(50) NOT NULL,
+fecha_pago DATETIME DEFAULT GETDATE() NOT NULL,
+CONSTRAINT FK_pedido_id_pagos FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+CONSTRAINT FK_metodo_pago_id_pagos FOREIGN KEY (metodo_pago_id) REFERENCES metodo_pago(id)
+);
+
+--Create ENVIOS
+CREATE TABLE envios(
+id INT PRIMARY KEY IDENTITY(1,1),
+pedido_id INT,
+direccion_envio VARCHAR(255) NOT NULL,
+ciudad_envio VARCHAR(100) NOT NULL,
+departamento VARCHAR(100) NOT NULL,
+referencia VARCHAR(100) NOT NULL,
+estado_envio VARCHAR(50) NOT NULL,
+fecha_envio DATETIME DEFAULT GETDATE(),
+fecha_entrega DATETIME,
+CONSTRAINT FK_pedido_id_envios FOREIGN KEY (pedido_id) REFERENCES pedidos(id)
+);
+
+--Create STOCK_TIENDA
+CREATE TABLE stock_tienda (
+id INT PRIMARY KEY IDENTITY(1,1),
+producto_id INT NOT NULL,
+tienda_id INT NOT NULL,
+cantidad INT NOT NULL,
+CONSTRAINT FK_producto_id_stock_tienda FOREIGN KEY (producto_id) REFERENCES productos(id),
+CONSTRAINT FK_tienda_id_stock_tienda FOREIGN KEY (tienda_id) REFERENCES tiendas(id)
+);
+
+
+
+
+
+ALTER TABLE envios
+ADD CONSTRAINT check_tipo_persona CHECK (tipo_persona IN ('Persona Natural','Persona Jurídica'));
